7565ab51605f235c38580a7f9d4bc093995411d0
Daniel Hatas <21317298+samson84@users.noreply.github.com>
2024-09-25 19:23:44 +0200

    ignore todos.json and create it automatically if not exists.

--- src/file-store.js
+++ src/file-store.js
@@ -1,6 +1,10 @@
-import {readFileSync, writeFileSync} from 'node:fs';
+import {existsSync, readFileSync, writeFileSync} from 'node:fs';
 
 export function createStore(path) {
+  if(!existsSync(path)) {
+    writeFileSync(path, JSON.stringify([], null, 2), 'utf-8');
+  } 
+
   return {
     get: () => {
       const textContent = readFileSync(path, 'utf-8');
